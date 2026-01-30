07f4de5bfbd0079942a5167c7bef223fa6e3c262
Jagruti <jagrutitak154730@gmail.com>
2026-01-30 21:43:00 +0530

    fix multer upload path

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "public/uploads");
+    cb(null, path.join(__dirname, "public/uploads"));
   },
   filename: function (req, file, cb) {
     const uniqueName = Date.now() + "-" + file.originalname;
     cb(null, uniqueName);
   }
 });
+
 
 const upload = multer({
   storage: storage,
